348c981cd2c9fa301cbf6ada589b27a3edb0cdfc
Gianfresco <gianfranco.cavallini@edu.unife.it>
2024-09-30 12:38:20 +0200

    20240909 - es1 funzionante

---
+++ 09-09-2024/es1/Esercizio1.java
@@ -0,0 +1,64 @@
+package es1;
+
+import java.io.*;
+import java.time.Year;
+
+public class Esercizio1 {
+    public static void main(String args[]) {
+        PipedInputStream pis = new PipedInputStream();
+        PipedOutputStream pos = null;
+
+        try {
+            pos = new PipedOutputStream(pis);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            System.exit(-1);
+        }
+
+        GeneraDati gd = new GeneraDati(pos);
+        Thread tgd = new Thread(gd);
+        tgd.start();
+
+        ObjectInputStream dis = null;
+
+        try {
+            dis = new ObjectInputStream(pis);
+        } catch (IOException e2) {
+            e2.printStackTrace();
+            System.exit(-2);
+        }
+
+        // controllo umidità
+        boolean first = true;
+        int umiditaPrecedente = 0;
+
+        while (true) {
+            Misure mis = null;
+
+            try {
+                mis = (Misure)dis.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                System.err.println(e);    
+            }
+
+            int umidita = mis.getUmidita();
+
+            if (!first) {
+                float variazione = (float) Math.abs(umidita - umiditaPrecedente) / umiditaPrecedente;
+                System.out.println("Variazione umidità registrata: " + variazione);
+                
+                if (variazione > 0.2) {
+                    System.out.println("Variazione superiore al 20%! -> TERMINAZIONE");
+                    gd.finisci();
+                    break;
+                }
+
+            } else {
+                first = false;
+            }
+
+            umiditaPrecedente = umidita;
+
+        }
+    }
+}
---
+++ 09-09-2024/es1/GeneraDati.java
@@ -0,0 +1,53 @@
+package es1;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PipedOutputStream;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class GeneraDati implements Runnable{
+    private AtomicBoolean isRunning = new AtomicBoolean(false);
+    private PipedOutputStream pos = null;
+
+    public GeneraDati (PipedOutputStream pos) {
+        this.pos = pos;
+    }
+
+    public void run() {
+        Random random = new Random();
+        isRunning.set(true);
+
+        ObjectOutputStream oos = null;
+
+        try {
+            oos = new ObjectOutputStream(pos);
+        } catch (IOException e) {
+            System.err.println(e);
+            System.exit(-30);
+        }
+
+        while (isRunning.get()) {
+            float tmp = random.nextFloat() * 25 - 5;    // float casuale da -5 a 25
+            int umd = random.nextInt(70) + 15;
+
+            Misure misure = new Misure(tmp, umd);
+            System.out.println("GeneraDati--- Temp: " + tmp + " Umidità: " + umd);
+
+            try {
+                oos.writeObject(misure);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Thread.sleep(2500);
+            } catch (InterruptedException ex) {}
+        }
+    }
+
+    public void finisci() {
+        isRunning.set(false);
+    }
+
+}
