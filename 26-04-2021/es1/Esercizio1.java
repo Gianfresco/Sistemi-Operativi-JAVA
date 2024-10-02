5ab7378c47b2c4e0cd147f04b3895b079103e45f
gianfresco <gianfranco.cavallini@edu.unife.it>
2024-10-02 15:55:13 +0200

    20210426 - es1 funzionante

--- 26-04-2021/es1/Esercizio1.java
+++ 26-04-2021/es1/Esercizio1.java
@@ -14,10 +14,38 @@ public class Esercizio1 {
             long oldTime = 0;
 
             for (int i = 0; i < 8; ++i) {
-                int val
+                int val = Integer.parseInt(br.readLine());
+                long newTime = Long.parseLong(br.readLine());
+
+                if (val < 20) {
+                    System.out.println(val + " - basso");
+                } else if (val < 75) {
+                    System.out.println(val + " - medio");
+                } else {
+                    System.out.println(val + " - alto");
+                }
+
+                long elapsedTime = newTime + oldTime;
+                if (elapsedTime > 15 && oldTime != 0) {
+                    System.out.println("ATTENZIONE: " + elapsedTime);
+                    oldTime = newTime;
+                }
             }
-        } catch (Exception e) {
             
+            ra.fermaRilevatore();
+
+            try {
+                ra.join();
+                System.out.println("Il rilevatore è stato terminato.");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Il processo principale è stato completato.");
+            br.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
