034024438fb789cfaf9a34b473234b6744e786ff
Gianfresco <gianfranco.cavallini@edu.unife.it>
2024-10-24 15:07:26 +0200

    pkg09_07_2021 proseguito

--- pkg09_07_2021/SimulaConsumi.java
+++ pkg09_07_2021/SimulaConsumi.java
@@ -1,5 +1,16 @@
 package pkg09_07_2021;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class SimulaConsumi implements Runnable {
-    
+    private AtomicBoolean isRunning = new AtomicBoolean(false);
+    private Consumi cns = null;
+
+    private SimulaConsumi(Consumi cns) {
+        this.cns = cns;
+    }
+
+    public void run() {
+        
+    }
 }
