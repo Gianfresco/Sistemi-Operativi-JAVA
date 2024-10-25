9c4040aeaf3791f54341297840b5fc43b76ad5f8
Gianfresco <gianfranco.cavallini@edu.unife.it>
2024-10-25 11:48:33 +0200

    pkg09_07_2021 funzionante

--- pkg09_07_2021/Main.java
+++ pkg09_07_2021/Main.java
@@ -1,5 +1,52 @@
 package pkg09_07_2021;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Consumi cns = new Consumi();
+        SimulaConsumi simCns = new SimulaConsumi(cns);
+        Thread TsimCns = new Thread(simCns);
+        TsimCns.start();
+
+        int count = 0;
+        float precedente = 0.0F;
+        float attuale = 0.0F;
+
+        while (true) {
+            try {
+                Thread.sleep(900);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            attuale = cns.getConsumi();
+            System.out.println("Consumo rilevato: " + attuale);
+
+            if (attuale > precedente) {
+                if ((attuale - precedente) > (precedente * 30) / 100) {
+                    System.out.println("Attenzione: variazione consumi oltre il 30%!");
+
+                }
+            }
+
+            if (attuale > 20) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            if (count == 3) {
+                System.out.println("Rilevati 3 valori consecutivi troppo alti -> Terminazione");
+                simCns.termina();
+                break;
+            }
+
+            precedente = attuale;
+        }
+
+        try {
+            TsimCns.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- pkg09_07_2021/SimulaConsumi.java
+++ pkg09_07_2021/SimulaConsumi.java
@@ -6,11 +6,25 @@ public class SimulaConsumi implements Runnable {
     private AtomicBoolean isRunning = new AtomicBoolean(false);
     private Consumi cns = null;
 
-    private SimulaConsumi(Consumi cns) {
+    public SimulaConsumi(Consumi cns) {
         this.cns = cns;
     }
 
     public void run() {
-        
+        isRunning.set(true);
+
+        while (isRunning.get()) {
+            cns.setConsumi((float) (Math.random() * 30));
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void termina() {
+        isRunning.set(false);
     }
 }
