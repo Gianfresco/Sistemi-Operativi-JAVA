02fed44c84e4122d82b3d32247d944a8dea59d67
gianfresco <gianfranco.cavallini@edu.unife.it>
2024-10-29 18:25:27 +0100

    esame8 - stream funzionante

--- esame8/stream/MacchinaA.java
+++ esame8/stream/MacchinaA.java
@@ -1,5 +1,53 @@
 package esame8.stream;
 
-public class MacchinaA {
-    // TODO
+import java.io.IOException;
+import java.io.PipedOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class MacchinaA implements Runnable {
+    private AtomicBoolean isRunning = new AtomicBoolean(false);
+    private PipedOutputStream pos = null;
+
+    public MacchinaA(PipedOutputStream pos) {
+        this.pos = pos;
+    }
+
+    public void run() {
+        isRunning.set(true);
+
+        while (isRunning.get()) {
+            System.out.println("Inizio lavorazione prodotto...");
+            
+            try {
+                Thread.sleep(200);
+                System.out.println("Fine lavorazione prodotto");
+
+                byte[] buffer = null;
+                String pg = "prodotto-grezzo";
+
+                try {
+                    buffer = pg.getBytes("UTF-8");
+
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    pos.write(buffer);
+                    pos.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(-1);
+                }
+                
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void termina() {
+        isRunning.set(false);
+    }
 }
--- esame8/stream/Main.java
+++ esame8/stream/Main.java
@@ -1,5 +1,43 @@
+
 package esame8.stream;
 
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
 public class Main {
-    // TODO
+    public static void main(String[] args) {
+        PipedInputStream pis = new PipedInputStream();
+        PipedOutputStream pos = null;
+
+        try {
+            pos = new PipedOutputStream(pis);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        MacchinaA mA = new MacchinaA(pos);
+        Thread TmA = new Thread(mA);
+        TmA.start();
+
+        MacchinaB mB = new MacchinaB(pis);
+        Thread TmB = new Thread(mB);
+        TmB.start();
+
+        try {
+            Thread.sleep(60 * 1000);
+            mA.termina();
+            mB.termina();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            TmA.join();
+            TmB.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
